686380af72fce4f359001079012f666caec37dc9
louis-lin <louis.lou.lin@gmail.com>
2024-07-05 11:08:03 -0600

    Update to Press, add copy button and adjust line spacing

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,8 +11,8 @@ release = '0.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
-    'sphinx_rtd_theme',
     'sphinxcontrib.mermaid',
+    'sphinx_copybutton',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -23,9 +23,21 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'press'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+html_theme_options = {
+    'show_sidebar_index': True,  # This should show the full tree
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Custom CSS file
+html_css_files = [
+    'custom.css',
+]
